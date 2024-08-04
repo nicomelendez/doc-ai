ccdac845d898ee62deac989753023da15cfc9187
nicomelendez <nicomelendez10@gmail.com>
2024-08-04 02:07:12 -0300

    Controlar si tiene animaciones activadas

--- src/components/utils/Title.jsx
+++ src/components/utils/Title.jsx
@@ -2,12 +2,25 @@ import React from 'react'
 import '@github/typing-effect-element'
 
 export default function Title() {
+  const prefersReducedMotion = window.matchMedia(
+    '(prefers-reduced-motion: reduce)'
+  ).matches
   return (
-    <h1 className='text-2xl xs:text-3xl md:h-auto text-white font-bold sm:text-4xl leading-relaxed pb-3'>
-      <typing-effect data-lines='["Obten un documento inicial en unos clicks"]'>
-        <span data-target='typing-effect.content'></span>
-        <span data-target='typing-effect.cursor'>|</span>
-      </typing-effect>
-    </h1>
+    <div class='mx-auto text-center max-w-[630px] lg:max-w-[800px] min-h-[8.5rem] md:min-h-[5rem]'>
+      <h1 className='text-2xl xs:text-3xl md:h-auto text-white font-bold sm:text-4xl leading-relaxed pb-3'>
+        <typing-effect data-lines='["Obten un documento inicial en unos clicks"]'>
+          <span data-target='typing-effect.content'></span>
+          <span data-target='typing-effect.cursor'>|</span>
+        </typing-effect>
+      </h1>
+      <p
+        className={`text-slate-300 text-sm lg:text-base text-pretty ${
+          prefersReducedMotion ? '' : 'animate-fade-in animate-delay-[2s]'
+        }`}
+      >
+        Proporciona la información sobre un tema y obtén un documento Word para
+        seguir editando.
+      </p>
+    </div>
   )
 }
