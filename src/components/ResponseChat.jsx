0c0cd8181f25da1e870659441ba7db8dc9e91350
nicomelendez <nicomelendez10@gmail.com>
2024-08-19 14:39:23 -0300

    Sacar modo dark

--- src/components/ResponseChat.jsx
+++ src/components/ResponseChat.jsx
@@ -30,8 +30,8 @@ export default function ResponseChat({ responseText }) {
         {responseText && (
           <div className='flex items-start gap-2.5 w-full pb-10'>
             <div className='flex flex-col gap-1 w-full max-w-[720px] mx-auto'>
-              <div className='flex flex-col leading-1.5 p-4 border-gray-200 bg-gray-100 rounded-e-xl rounded-es-xl dark:bg-gray-700'>
-                <p className='text-sm font-normal text-gray-900 dark:text-white'>
+              <div className='flex flex-col leading-1.5 p-4 border-gray-200 rounded-e-xl rounded-es-xl bg-gray-700'>
+                <p className='text-sm font-normal text-white'>
                   {responseText}
                 </p>
               </div>
